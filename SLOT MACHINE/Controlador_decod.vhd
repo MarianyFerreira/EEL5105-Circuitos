c9fd8fae7cb2274eab3eb7494aed1f17b74a0d65
Mariany Ferreira da Silva <marianyufsc@gmail.com>
2016-03-31 14:36:59 -0300

    indentation code

--- SLOT MACHINE/Controlador_decod.vhd
+++ SLOT MACHINE/Controlador_decod.vhd
@@ -2,14 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Controlador_decod is
-port (A: in std_logic_vector (3 downto 0);
-			F: out std_logic_vector(6 downto 0)
-			);
-		end Controlador_decod;
+	port (  A: in std_logic_vector (3 downto 0);
+		F: out std_logic_vector(6 downto 0)
+      	);
+end Controlador_decod;
 
-		architecture ARCH of Controlador_decod is
-		begin
-	 F <= "1000000" when A = "0000" else -- 0 x '30'
+architecture ARCH of Controlador_decod is
+	begin
+		   F <= "1000000" when A = "0000" else -- 0 x '30'
 			"1111001" when A = "0001" else -- 1
 			"0100100" when A = "0010" else -- 2
 			"0110000" when A = "0011" else -- 3
@@ -26,4 +26,4 @@ port (A: in std_logic_vector (3 downto 0);
 			"0000110" when A = "1110" else -- E
 			"0001110" when A = "1111" else -- F x'46'
 			"1111111";
-				end ARCH;
+end ARCH;
